f5d502f84de57afd39491b1523b50e454d20fe5d
Mithil Hassan <mithilhassan007@gmail.com>
2022-12-27 22:09:33 +0600

    Register and Login page added

---
+++ src/pages/Register.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Add from "../img/addAvatar.png";
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="Display Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input
+            style={{ display: "none" }}
+            type="file"
+            placeholder="file"
+            id="file"
+          />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
